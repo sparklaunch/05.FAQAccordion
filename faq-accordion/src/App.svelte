15c06be7804e1db2f0f7f75df9e4fb8014297171
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-15 10:17:42 +0900

    Add Typescript support

--- faq-accordion/src/App.svelte
+++ faq-accordion/src/App.svelte
@@ -1,6 +1,9 @@
+<script lang="ts">
+</script>
+
 <svelte:head>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link
         href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap"
         rel="stylesheet"
--- faq-accordion/src/main.ts
+++ faq-accordion/src/main.ts
@@ -1,7 +1,7 @@
 import App from "./App.svelte";
 
-const app = new App({
-    target: document.body,
+const app: App = new App({
+    target: document.body
 });
 
 export default app;
